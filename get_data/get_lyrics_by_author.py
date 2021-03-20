0ee27af270e3300305a3d85f18e79854d81fb0d7
Lushi Chen <s1690903@hawksworth.inf.ed.ac.uk>
2021-03-20 21:53:39 +0000

    added tags

--- get_data/get_lyrics_by_author.py
+++ get_data/get_lyrics_by_author.py
@@ -1,60 +1,125 @@
-from rap_stats import *
+import time 
+import os
+import csv
+import pandas as pd
+from ruamel import yaml
+import lyricsgenius
+import json
+import collections
+import requests
 
 
+def load_experiment(path_to_experiment):
+    #load experiment 
+    data = yaml.safe_load(open(path_to_experiment))
+    return data
 
 class Collect_artist_songs:
-    def __init__(self, artist_path, billb_path, clean_artist, token):
+    def __init__(self, path, outputfile, artist_list_path, token):
         '''define the main path'''
-        self.path = '/disk/data/share/s1690903/Lyrics_project/'
-        self.artist_path = artist_path #where you save the artist file
-        self.billb_path = billb_path 
+        self.path = path
+        self.outputfile = outputfile #where you save the artist file
+        self.artist_list_path = artist_list_path
         self.genius_client_access_token = token
-        self.clean_artist_path = clean_artist
+        #self.clean_artist_path = clean_artist
 
     def get_authors(self):
         genius = lyricsgenius.Genius(self.genius_client_access_token)
-        bill = pd.read_csv(self.path + self.billb_path)
+        artists = pd.read_csv(self.path + self.artist_list_path)
         # drop artist duplicates
-        bill = bill.drop_duplicates(subset=['artist'])
-        bill["artist_id"] = bill.index + 1
-        bill.to_csv(self.path + self.clean_artist_path)
+        artists = artists.drop_duplicates(subset=['artist_id'])
+        #artists.to_csv(self.path + 'cleaned_artists.csv')
+        artists = artists.iloc[68::]
 
-        file_exists = os.path.isfile(self.path + self.artist_path)
-        f = open(self.path + self.artist_path, 'a')
+        #artists["artist_id"] = artists.index + 1
+        #bill.to_csv(self.path + self.clean_artist_path)
+
+        file_exists = os.path.isfile(self.path + self.outputfile)
+        f = open(self.path + self.outputfile, 'a')
         writer_top = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+
         if not file_exists:
-            writer_top.writerow(['title'] + ['artist'])
+            writer_top.writerow(['title'] + ['artist_name'] + ['lyrics'] + ['year'] + ['artistid'])
             f.close()
 
-        for artistid, artist in zip(bill['artist_id'], bill['artist']):
-            try:
-                artist = genius.search_artist(artist, max_songs=500, sort="title")
+        else:
 
-                f = open(self.path + self.artist_path, 'a')
-                writer_top = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+            lyric_dict = collections.defaultdict(dict)
 
-                if artist is not None:
-                    num_songs = len(artist)
-                    for i in range(0, num_songs-1):
-                        print("*********************", artist.songs[i].title)
-                        result_row = [[artist.songs[i].title, artist.songs[i].artist]]
-                        writer_top.writerows(result_row)
+            for artistid, artist in zip(artists['artist_id'], artists['name']):
 
-            except TypeError:
+                # get max 200 songs from each artist
+                try:
+                    artist_songs = genius.search_artist(artist, max_songs=500, sort="title")
+                    #time.sleep()
 
-                    pass
-        f.close()
+                except (requests.exceptions.ChunkedEncodingError, requests.ReadTimeout, requests.exceptions.ConnectionError, TypeError) as err:
 
+                    time.sleep(5 * 60)
+                    continue
+
+
+                if artist_songs is not None:# check if object is empty
+
+                    # store ids in dict
+                    lyric_dict[artistid]['name'] = artist
+                    #lyric_dict['artist_id']['artist_name'] = [artist]
+
+                    # store info in csv
+                    f = open(self.path + self.outputfile, 'a')
+                    writer_top = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+                    try:
+                        # store search as csv
+                        # store all the songs from artist object
+                        
+                        for i in artist_songs.songs:
+
+                            lyric_dict[artistid]['song_title'] = i.title
+                
+                            song_lyrics = artist_songs.song(i.title)
+                            print(song_lyrics.lyrics)
+
+                            lyric_dict[artistid]['song_lyrics'] = song_lyrics.lyrics
+
+                            if song_lyrics.year is not None:
+                                print('year exists')
+
+                                lyric_dict[artistid]['year'] = song_lyrics.year
+                                #print ('this is result', lyric_dict)
+
+                            result_row = [[i.title, i.artist, song_lyrics.lyrics, song_lyrics.year, artistid]]
+                            writer_top.writerows(result_row)
+                            
+                            # store result to json
+                            with open(self.path + '{}.json'.format(self.outputfile), 'a') as fjson:
+                                json.dump(lyric_dict, fjson)
+                                fjson.close()
+
+                    except TypeError:
+                        continue
+
+                    f.close()
+                
+
+        
 
 
 experiment = load_experiment('/disk/data/share/s1690903/Lyrics_project/' + 'env/experiment.yaml')
-# art = Collect_artist_songs(artist_path='data/artist_1980s.csv', billb_path='data/cleaned_hothp_1980s.csv', clean_artist = 'data/clean_artist_1980s.csv', token = experiment['token'][2])
-# art.get_authors()
+path = '/disk/data/share/s1690903/Lyrics_project_git/data/'
+outputfile = 'songs_year.csv'#name of the song file
+#artist_list_path = 'artist_list_rap_US_filtered3.csv'
+artist_list_path = 'cleaned_artists.csv'
+genius_client_access_token = experiment['token'][2]
 
 
+art = Collect_artist_songs(artist_list_path=artist_list_path, outputfile=outputfile, path=path, token = genius_client_access_token)
+art.get_authors()
 
-genius = lyricsgenius.Genius(experiment['token'][4])
-artist = genius.search_artist("YoungBoy Never Broke Again", max_songs=3, sort="title")
+
+# genius = lyricsgenius.Genius(experiment['token'][4])
+# artist = genius.search_artist("YoungBoy Never Broke Again", max_songs=3, sort="title")
 
 
 
--- get_data/musicbrainz.py
+++ get_data/musicbrainz.py
@@ -4,31 +4,25 @@ import os
 import csv
 import time
 import json
+from ruamel import yaml
+import collections
+import pandas as pd
 
 #https://musicbrainz.org/doc/MusicBrainz_API/Search
 
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--cdrom", help="provide the source of the cd", default="/dev/cdrom")
-# args = parser.parse_args()
-
-# device = args.cdrom
-
-# print("device: %s" % device)
-# disc = discid.read(device)
-# print("id: %s" % disc.id)
-
-# result = musicbrainzngs.get_releases_by_discid(disc.id, includes=["artists"])
-
-
+def load_experiment(path_to_experiment):
+    #load experiment
+    data = yaml.safe_load(open(path_to_experiment))
+    return data
 
 
 class CollectReplies:
     """Collect replies via twitter api v2."""
 
-    def __init__(self, inputP, outputP, outputFile, tag, country, filter_tags):
+    def __init__(self, outputP, outputFile, tag, country, filter_tags):
         '''define the main path'''
-        self.inputP = inputP# input path
+       
         self.outputPath = outputP# output path
         self.outputFile = outputFile
         self.tag = tag
@@ -39,8 +33,11 @@ class CollectReplies:
         """Search music in musicbrainz"""
 
         #By default the web service returns 25 results per request and you can set a limit of up to 100. You have to use the offset parameter to set how many results you have already seen so the web service doesn’t give you the same results again.
+        if self.country is not None:
+            result = musicbrainzngs.search_artists(query='', limit=100, offset=offset, strict=False, country=self.country, tag=self.tag)
 
-        result = musicbrainzngs.search_artists(query='', limit=100, offset=offset, strict=False, tag=self.tag, country=self.country)
+        else:
+            result = musicbrainzngs.search_artists(query='', limit=100, offset=offset, strict=False, tag=self.tag)
 
         time.sleep(1)
 
@@ -57,6 +54,7 @@ class CollectReplies:
 
         return result
 
+
     def store_result_artist(self, offset):
 
         search_result = self.search_artist(offset)
@@ -71,35 +69,79 @@ class CollectReplies:
 
         # query user profile for each handle
         if file_exists:
-            with open(self.outputPath + '{}.json'.format(self.outputFile), 'a') as f:
-                json.dump(search_result, f)
+            # with open(self.outputPath + '{}.json'.format(self.outputFile), 'a') as f:
+            #     json.dump(search_result, f)
 
             f = open(self.outputPath + '{}.csv'.format(self.outputFile), 'a', encoding='utf-8-sig')
             writer_top = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 
-
-
+            prev_artist = None
             for artist in search_result['artist-list']:
                 if 'tag-list' in artist.keys():
-                    for tag in artist['tag-list']:
-                        if tag['name'] in self.filter_tags:
-                # here store data for each artist
+                    for tag in artist['tag-list']:      
+                        if (tag['name'] in self.filter_tags.split(',')) & (artist['id'] != prev_artist):
+                            #print(tag['name'])
+            # here store data for each artist
                             try:
-                                if ('disambiguation' in artist.keys()) & ('country' not in artist.keys()):
-                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], None, artist['life-span']['begin'], artist['life-span']['ended'], artist['disambiguation']]]
+                                
+                                if ('disambiguation' in artist.keys()) & ('country' not in artist.keys()) & ('gender' not in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], None, None, None, None, artist['disambiguation']]]
 
-                                if ('disambiguation' not in artist.keys()) & ('country' in artist.keys()):
-                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], artist['country'], artist['life-span']['begin'], artist['life-span']['ended'], None]]
+                                elif ('disambiguation' in artist.keys()) & ('country' not in artist.keys()) & ('gender' not in artist.keys()) & ('life-span' in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], None, None, artist['life-span']['begin'], artist['life-span']['ended'], artist['disambiguation']]]
 
-                                if ('disambiguation' in artist.keys()) & ('country' in artist.keys()):
+
+                                elif ('disambiguation' in artist.keys()) & ('country' in artist.keys()) & ('gender' not in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'],None, artist['country'], None, None, artist['disambiguation']]]
+
+                                elif ('disambiguation' in artist.keys()) & ('country' in artist.keys()) & ('gender' in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], artist['country'], None, None,artist['life-span']['ended'], artist['disambiguation']]]
+
+                                elif ('disambiguation' in artist.keys()) & ('country' in artist.keys()) & ('gender' not in artist.keys()) & ('life-span' in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], None, artist['country'], artist['life-span']['begin'], artist['life-span']['ended'], artist['disambiguation']]]
+
+                                elif ('disambiguation' in artist.keys()) & ('country' in artist.keys()) & ('gender' in artist.keys()) & ('life-span' in artist.keys()):
                                     content = [[artist['id'], artist['type'], artist['name'], artist['gender'], artist['country'], artist['life-span']['begin'], artist['life-span']['ended'], artist['disambiguation']]]
 
-                                else:
+                                elif ('disambiguation' in artist.keys()) & ('country' not in artist.keys()) & ('gender' in artist.keys()) & ('life-span' in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], None, artist['life-span']['begin'], artist['life-span']['ended'], artist['disambiguation']]]
+
+                                elif ('disambiguation' in artist.keys()) & ('country' not in artist.keys()) & ('gender' in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], None, None, None, artist['disambiguation']]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' in artist.keys()) & ('gender' in artist.keys()) & ('life-span' in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], None, None, artist['life-span']['begin'], artist['life-span']['ended'], None]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' not in artist.keys()) & ('gender' in artist.keys()) & ('life-span' in artist.keys()):
                                     content = [[artist['id'], artist['type'], artist['name'], artist['gender'], None, artist['life-span']['begin'], artist['life-span']['ended'], None]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' in artist.keys()) & ('gender' in artist.keys()) & ('life-span' in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], artist['country'], artist['life-span']['begin'], artist['life-span']['ended'], None]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' in artist.keys()) & ('gender' not in artist.keys()) & ('life-span' in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], None, artist['country'], artist['life-span']['begin'], artist['life-span']['ended'], None]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' in artist.keys()) & ('gender' in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], artist['country'], None, None, None]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' not in artist.keys()) & ('gender' in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], artist['gender'], None, None, None, None]]
+
+                                elif ('disambiguation' not in artist.keys()) & ('country' in artist.keys()) & ('gender' not in artist.keys()) & ('life-span' not in artist.keys()):
+                                    content = [[artist['id'], artist['type'], artist['name'], None, artist['country'],  None, None, None, None]]
+
+
+                                else:
+                                    content = [[artist['id'], artist['type'], artist['name'], None, None, None, None, None]]
+                            
+                                
+                                prev_artist = artist['id']
+                                
+                                writer_top.writerows(content)
+
                             except KeyError:
                                 continue
-
-                            writer_top.writerows(content)
+                          
 
             return search_result
 
@@ -117,9 +159,6 @@ class CollectReplies:
 
         # query user profile for each handle
         if file_exists:
-            with open(self.outputPath + '{}.json'.format(self.outputFile), 'a') as f:
-                json.dump(search_result, f)
-
             f = open(self.outputPath + '{}.csv'.format(self.outputFile), 'a', encoding='utf-8-sig')
             writer_top = csv.writer(f, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 
@@ -143,10 +182,15 @@ class CollectReplies:
         artist_count = search_result['artist-count']
         print('there are {} artists in the db'.format(artist_count))
 
-        for offset in range(0, artist_count, 100):
+        multidicts = []
+        for offset in range(0, 10000, 100):
             print('this is loop {}'.format(offset))
 
             search_result = self.store_result_artist(offset)
+            multidicts.append(search_result)
+
+        with open(self.outputPath + '{}.json'.format(self.outputFile), 'a') as f:
+                json.dump(multidicts, f)
 
         return search_result
 
@@ -166,24 +210,107 @@ class CollectReplies:
 
         return search_result
 
+class GetOtherInfo:
+    """From here you can retrieve other variables from the json file"""
+    def __init__(self, inputP, inputFile, outputP, outputFile_newVar):
+        '''define the main path'''
+        self.inputP = inputP
+        self.inputFile = inputFile
+        self.outputPath = outputP# output path
+        self.outputFile = outputFile_newVar
+    
+    def Read_json(self):
+        """Read json to dict """
 
-inputP = '/disk/data/share/s1690903/Lyrics_project_git/data/'
+        with open(self.inputP + self.inputFile, encoding="utf8") as f:
+            
+            data = f.readlines()
+
+            data = [json.loads(line) for line in data] #convert string to dict format
+
+        return data
+
+
+    def get_tags(self):
+        """retrieve tags from the profile doc """
+
+        artists_result = self.Read_json()
+
+        tag_dfs = pd.DataFrame()
+        for artists in artists_result[0]:
+            if artists is not None:
+
+                tag_dict = collections.defaultdict(dict)
+                for art in artists['artist-list']:
+                    if 'tag-list' in art.keys():
+
+                        prev_count = 0
+                        prev_tag_list = ''
+                
+                        for each_tag in art['tag-list']:
+                            # combine a list of tags
+    
+                            tags_list = each_tag['name'] + ', ' + prev_tag_list
+                            prev_tag_list = tags_list
+
+                            if int(each_tag['count']) > int(prev_count):
+                                tag_dict[art['id']]['max_count'] = each_tag['count']
+                                tag_dict[art['id']]['max_tag'] = each_tag['name']
+
+                                prev_count = each_tag['count']
+
+                        tag_dict[art['id']]['tags_list'] = tags_list
+
+                        tag_df = pd.DataFrame.from_dict(tag_dict, orient='index')
+                        if tag_df is not None:
+                            tag_dfs = tag_dfs.append(tag_df)
+
+        #tag_df.columns = ['']
+        tag_dfs.to_csv(self.outputPath + self.outputFile, encoding='utf-8-sig')
+
+        return tag_dfs
+
+    def merge_files(self, path, file1_name, file2_name, outputFile):
+        """merge other variables with artist list """
+
+        file1 = pd.read_csv(path + file1_name)
+        file1.columns = ['artist_id', 'max_count', 'max_tag', 'tags_list']
+        file1 = file1.drop_duplicates(subset=['artist_id'])
+
+        file2 = pd.read_csv(path + file2_name)
+
+        all_file = pd.merge(file1, file2, on='artist_id', how='inner')
+
+        all_file.to_csv(path + outputFile, encoding='utf-8-sig')
+
+        return all_file
+        
+
+
+
+
+script_path = '/disk/data/share/s1690903/Lyrics_project_git/get_data/'
 outputP = '/disk/data/share/s1690903/Lyrics_project_git/data/'
-outputFile = 'artist_list_rap_US_filtered3'
-tag = 'rap'# search by tags
-country = 'US'
-filter_tags = 'rap, r&b, hip hot, hip-hot, rnb'# filter out tags
+outputFile = 'artist_list_setting2'
 
+
+env = load_experiment(script_path + 'parameters.yaml')
 musicbrainzngs.set_useragent("Audacious", "0.1", "https://github.com/jonnybarnes/audacious")
 
-cr = CollectReplies(inputP=inputP, outputP=outputP, outputFile=outputFile, tag=tag, country=country, filter_tags=filter_tags)
-#result = cr.store_result(100)
+cr = CollectReplies(outputP=outputP, outputFile=outputFile, tag=env['settings2']['tag'], country=None, filter_tags=env['settings2']['filter_tags'])
+
 result = cr.loop_file_artist()
-# result = musicbrainzngs.search_artists(query='', limit=100, offset=200, strict=False, tag='hip hot', country='US')
+#result = musicbrainzngs.search_artists(query='', limit=10, offset=200, strict=False, tag='hip hop', country='US')
 
-#result = cr.loop_file_songs()
-# result = musicbrainzngs.search_recordings(query='', limit=10, offset=20, strict=False, tag='hip hot', country='US')
 
+####get other variables
+inputFile_json = 'artist_list_setting2.json'
+outputFile_newVar = 'tags.csv'
+other = GetOtherInfo(inputP=outputP, outputP=outputP, inputFile=inputFile_json, outputFile_newVar=outputFile_newVar)
+
+tags = other.get_tags()
+
+other.merge_files(path=outputP, file1_name='tags.csv', file2_name='artist_list_setting2.csv', outputFile='artist_list_setting2_tags.csv')
 
 
 
